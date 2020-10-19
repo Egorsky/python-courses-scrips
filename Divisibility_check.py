8451e0322cb9bcf1af36928362a0b2ab00a34d91
egorski <egor.a.kolpakov@gmail.com>
2020-10-19 19:41:46 +0200

    A divisibility check with no loops, functions or branches

---
+++ Divisibility_check.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The program checks if the number A is evenly divisible by the number B."""
+
+
+a = int(input())
+b = int(input())
+yes = a % b  # a first check, returns 1 or 0
+f = 0**yes  # using this for the upcoming check. 0**0 = 1
+print(('Yes' * f) + ('No' * (1 - f)))  # because of upper circumstances, only part of the string can be printed
