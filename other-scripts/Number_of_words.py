35235868f3856391d2707daa2b9f6a1822b08cd4
egorski <egor.a.kolpakov@gmail.com>
2020-10-21 14:46:22 +0200

    Program counts the number of words in the string

---
+++ other-scripts/Number_of_words.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The user enters a string of words separated by spaces. 
+   The program determines how many words it contains."""
+
+
+s = input()
+b = s.count(' ')
+count = b + 1
+print(count)
