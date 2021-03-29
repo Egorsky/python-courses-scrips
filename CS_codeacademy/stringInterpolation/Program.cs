39960a79bcbf21ab444fa09575fe14cd72043ab9
Egorsky <egor.a.kolpakov@gmail.com>
2021-03-29 08:27:36 +0200

    Addeed a prog that checks pass for a spec.char and string interpolation

---
+++ CS_codeacademy/stringInterpolation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace stringInterpolation
+{
+    class Program
+    {
+        // Declare the variables
+        string beginning = "Once upon a time,";
+        string middle = "The kid climbed a tree";
+        string end = "Everyone lived happily ever after.";
+
+        // Interpolate the string and the variables
+        string story = $"{beginning} {middle} {end}";
+        Console.WriteLine(story);
+
+
+      // Print the story to the console 
+
+    }
+}
