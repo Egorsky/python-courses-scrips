28257784f18ec9a6586d752b05ea912a169995dd
Egorsky <egor.a.kolpakov@gmail.com>
2021-03-30 08:37:14 +0200

    Added program that are working with strings

---
+++ CS_codeacademy/NameGrab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NameGrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Name
+            string name = "Farhad Hesam Abbasi";
+
+            // Get first letter
+            int letterPos = name.IndexOf("F");
+            char firstLetter = name[letterPos];
+
+
+
+            // Get last name
+            int lastNCh = name.IndexOf("Abbasi");
+            string lastName = name.Substring(lastNCh);
+
+
+            // Print results
+            Console.WriteLine(firstLetter);
+            Console.WriteLine(lastName);
+
+
+        }
+    }
+}
---
+++ CS_codeacademy/StringCheck/StringCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string anyStr = "sdfsdfk111";
+            int anInd = anyStr.Length;
+
+            Console.WriteLine(anInd);
+
+            string poem = "just like in a dream";
+            int whInd = poem.IndexOf("d");
+            Console.WriteLine(whInd);
+            Console.WriteLine(poem.Substring(whInd));
+            Console.WriteLine(poem.Substring(whInd).ToUpper());
+
+
+        }
+    }
+}
