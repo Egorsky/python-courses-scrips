1ddfb692a91e54e6ac55048684bb7b680aa9b8ad
Egorsky <egor.a.kolpakov@gmail.com>
2021-04-30 16:17:27 +0200

    Added a program which uses return for the method

---
+++ CS_codeacademy/Return/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Return
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DecoratePlanet(planet: "Mars"));
+        }
+
+        static string DecoratePlanet(string planet)
+        {
+            return $"*..*..* Welcome to {planet} *..*..*";
+        }
+    }
+}
