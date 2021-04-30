1abce77254a748a78549356973172b8ebd8d56e9
Egorsky <egor.a.kolpakov@gmail.com>
2021-04-30 08:31:01 +0200

    Added new programs with method overloading, named arguments and optional param

---
+++ CS_codeacademy/MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamePets("Laika", "Albert");
+            NamePets("Mango", "Puddy", "Bucket");
+            NamePets();
+        }
+        static void NamePets(string pet1, string pet2)
+        {
+            Console.WriteLine($"Your pets {pet1} and {pet2} will be joining your voyage across space!");
+        }
+        static void NamePets(string pet3, string pet4, string pet5)
+        {
+            Console.WriteLine($"Your pets {pet3}, {pet4}, and {pet5} will be joining your voyage across space!");
+        }
+        static void NamePets()
+        {
+            Console.WriteLine("Aw, you have no spacefaring pets :(");
+        }
+    }
+}
---
+++ CS_codeacademy/NamedArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitPlanets(numberOfPlanets: 2, name: "Erik");
+        }
+
+        static void VisitPlanets(
+          string adjective = "brave",
+          string name = "Cosmonaut",
+          int numberOfPlanets = 0,
+          double gForce = 4.2)
+        {
+            Console.WriteLine($"Welcome back, {adjective} {name}.");
+            Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+            Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+        }
+    }
+}
