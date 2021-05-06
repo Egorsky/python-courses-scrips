524cbfba807abd0e0f37ea4e07e791ada7077614
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-06 18:02:10 +0200

    a program which uses while loop

---
+++ CS_codeacademy/WhileLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int emails = 20;
+            while (emails > 1)
+            {
+                emails = emails - 1;
+                Console.WriteLine($"Emails are deleting. {emails} left.");
+
+            }
+            Console.WriteLine("INBOX ZERO ACHIEVED!");
+        }
+    }
+}
