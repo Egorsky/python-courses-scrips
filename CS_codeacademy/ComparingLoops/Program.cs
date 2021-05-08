85ac03699f69651bdc00e77dc927b669c23545ca
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-08 08:19:01 +0200

    added a program with a loop

---
+++ CS_codeacademy/ComparingLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ComparingLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] websites = { "twitter", "facebook", "gmail" };
+
+
+            foreach (string site in websites)
+            {
+                Console.WriteLine(site);
+            }
+        }
+    }
+}
