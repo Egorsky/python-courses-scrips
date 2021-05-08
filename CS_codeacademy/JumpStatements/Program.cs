53b12679e4ec4310a91404876ea0f888f353e9eb
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-08 08:24:15 +0200

    added a program that uses do while and break in the loop

---
+++ CS_codeacademy/JumpStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JumpStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buttonClick = true;
+            int i = 0;
+            do
+            {
+                Console.WriteLine("BLARRRRR");
+                i++;
+                if (i == 3)
+                {
+                    break;
+                }
+            } while (!buttonClick);
+        }
+    }
+}
