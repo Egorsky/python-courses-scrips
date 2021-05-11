0a78793a0555b9f029372adc2538151140ef8be5
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-11 17:10:10 +0200

    a program that uses basics of classes. private and public members etc

---
+++ CS_codeacademy/BasicClasses/Forest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses
+{
+    class Forest
+    {
+        // Fields
+
+        public string name;
+        public int trees;
+        public int age;
+        private string biome;
+
+        // Properties 
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Trees
+        {
+            get { return trees; }
+
+            set { trees = value; }
+        }
+        public string Biome
+        {
+            get { return biome; }
+            set
+            {
+                if (value == "Tropical" ||
+                value == "Temperate" ||
+                value == "Boreal")
+                {
+                    biome = value;
+                }
+                else
+                {
+                    biome = "Unknown";
+                }
+
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+        public int Grow()
+        {
+            Trees += 30;
+            Age++;
+            return Trees;
+        }
+        public int Burn()
+        {
+            Trees -= 20;
+            Age++;
+            return Trees;
+        }
+        public Forest(int age)
+        {
+            this.Age = age;
+            ForestsCreated ++;
+        }
+        private static int forestsCreated;
+        public static int ForestsCreated
+        {
+            get { return forestsCreated; }
+            private set { forestsCreated = value; }
+        }
+
+    }
+}
---
+++ CS_codeacademy/BasicClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest(55000000);
+            f.Name = "Amazon";
+            f.Trees = 39000000;
+            //f.age = ;
+            f.Biome = "Tropical";
+            Console.WriteLine($"This forest is called the {f.Name}.");
+            Console.WriteLine(f.Biome);
+            Console.WriteLine(f.trees);
+            Console.WriteLine($"It's {f.age} years old");
+            f.Grow();
+            Console.WriteLine($"The number of trees increased. New number of trees is {f.Trees}");
+            f.Grow();
+            Console.WriteLine($"The number of trees increased. New number of trees is {f.Trees}");
+            f.Burn();
+            Console.WriteLine($"The number of trees decreased. New number of trees is {f.Trees}");
+
+        }
+    }
+}
