63b308439a21d979d9532ebaf63e3a035137cf50
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-11 17:11:24 +0200

    a program that uses static members

---
+++ CS_codeacademy/StaticMembers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.PI);
+            Console.WriteLine(Math.Abs(-32));
+        }
+    }
+}
+
