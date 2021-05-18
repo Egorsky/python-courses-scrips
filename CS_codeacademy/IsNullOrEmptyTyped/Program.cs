4aa7123deb2e8fb35974e61dcd89cbef54fd3f0b
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-18 06:18:38 +0200

    a program that checks if there was an imput from the user or not

---
+++ CS_codeacademy/IsNullOrEmptyTyped/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IsNullOrEmptyTyped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            if (String.IsNullOrEmpty(a))
+            {
+                Console.WriteLine("You didn't enter anything!");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for your submission!");
+            }
+
+        }
+    }
+}
