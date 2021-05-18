56615448f36b0df675a393fa3da464b37a1af87f
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-18 06:15:17 +0200

    a program that compares two strings and two Objects

---
+++ CS_codeacademy/StringTheException/StringTheException/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTheException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "immutable";
+            string b = "immutable";
+            Console.WriteLine(a == b);
+            Object c = new Object();
+            Object d = new Object();
+            Console.WriteLine(c == d);
+        }
+    }
+}
