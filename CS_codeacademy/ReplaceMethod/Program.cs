ee37a24b2681913c2c76b48e1d46217fd75e651b
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-18 06:19:35 +0200

    a program that uses Replace method

---
+++ CS_codeacademy/ReplaceMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReplaceMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lyrics =
+              "Dollie, Dollie, bo-bollie,\n" +
+              "Banana-fana fo-follie\n" +
+              "Fee-fi-mo-mollie\n" +
+              "Dollie!";
+
+           
+            string newLyrics = lyrics.Replace("ollie", "ana");
+
+            Console.WriteLine(newLyrics);
+            Console.WriteLine();
+            Console.WriteLine(lyrics);
+        }
+    }
+}
