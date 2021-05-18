5b480f2c7451cbe7bfb6141e7d487fd41b8fc23f
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-18 17:36:10 +0200

    a program that ueses linq sequence

---
+++ CS_codeacademy/VarInLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace VarInLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> heroes = new List<string> { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var shortHeroes = from h in heroes
+                              where h.Length < 8
+                              select h;
+            foreach (string n in shortHeroes)
+            {
+                Console.WriteLine(n);
+            }
+            var longHeroes = heroes.Where(n => n.Length > 8);
+            Console.WriteLine(longHeroes.Count());
+        }
+    }
+}
