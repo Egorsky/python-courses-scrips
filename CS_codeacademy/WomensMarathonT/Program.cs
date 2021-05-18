524b76ae0aa5a2e39300003fb11b4ae780cd3311
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-18 06:20:31 +0200

    a program that uses a list and apply Add method to it

---
+++ CS_codeacademy/WomensMarathonT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WomensMarathonT
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> marathons = new List<double>();
+            marathons.Add(144.07); //Jemima Sumgong
+            marathons.Add(143.12); // Tiki Gelana
+
+            Console.WriteLine(marathons[1]);
+
+        }
+    }
+}
