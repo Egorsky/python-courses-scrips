2c7488cf74d79a0e9bae61dd16bf2a1badf00512
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-19 18:09:59 +0200

    a program that uses linq for determening spaces in the list and unpacking

---
+++ CS_codeacademy/LinqSqus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSqus
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var introduct = heroes
+            .Select(h => $"Introducing...[{h.ToUpper()}]!");
+
+            var indx = from h in heroes
+                       where h.Contains(" ")
+                       select h.IndexOf(" ");
+
+            Console.WriteLine("\nresult 1:");
+            foreach (var s in introduct)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("\nresult 2:");
+            foreach (var s in indx)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
