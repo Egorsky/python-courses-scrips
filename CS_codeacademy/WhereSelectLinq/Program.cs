d617b48d01d8b8da754555e42e69aaa7e92df243
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-19 18:05:42 +0200

    a program which uses LINQ from-where-select statement

---
+++ CS_codeacademy/WhereSelectLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereSelectLinq
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+            var heroesWithI = from hr in heroes
+                              where hr.Contains("i")
+                              select hr;
+
+            var underscored = from hu in heroes
+                              select hu.Replace(" ", "_");
+
+            foreach (string s in heroesWithI)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("\nPrinting underscored: \n");
+
+            foreach (string s in underscored)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
