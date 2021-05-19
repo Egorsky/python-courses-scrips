c72cc825d0226fc0bc9c22d64187bd12b909ee79
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-19 18:08:21 +0200

    a program which uses LINQ as method with anonim functions

---
+++ CS_codeacademy/WhereSelectTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereSelectTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+            var hrwc = heroes.Where(h => h.Contains("c"));
+            var lowerHeroesWithC = hrwc.Select(h => h.ToLower());
+
+            var sameResult = heroes
+            .Where(h => h.Contains("c"))
+            .Select(h => h.ToLower());
+
+            foreach (string s in sameResult)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
