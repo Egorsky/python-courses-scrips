8b1748b0f235aff2c22fd033371a94385acd7716
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-19 18:11:32 +0200

    a progam which uses linq for the list

---
+++ CS_codeacademy/LnqListContains/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LnqListContains
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> heroesList = new List<string> { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var heronames = heroesList
+            .Where(h => h.Contains(".") || h.Contains("7"))
+            .Select(h => h);
+
+            foreach (var s in heronames)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
