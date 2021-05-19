b0481a4a0de27d2643668148be2783c6ef6d4513
Egorsky <egor.a.kolpakov@gmail.com>
2021-05-19 18:06:26 +0200

    a program which uses LINQ with anonim function inside

---
+++ CS_codeacademy/LinqAnonF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqAnonF
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            var heroesWithI = heroes.Where(h => h.Contains("i"));
+
+            foreach (string s in heroesWithI)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
